fef6c7aeb7d6e8c37e9362463ee1715bc50f23a7
Soheli Arefin Kona <119147072+Elham-Yadira@users.noreply.github.com>
2022-12-21 18:19:49 +0600

    Add files via upload

---
+++ Content Placeholder/cp.js
@@ -0,0 +1,31 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')        
+
+
+
+setTimeout(getData, 2500)
+
+
+
+function getData() {
+    header.innerHTML = '<img src="1.jpg" alt="laptop"></img>'
+    title.innerHTML = 'Lorem ipsum dolor sit amet.'
+    excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, pariatur.'
+    profile_img.innerHTML = '<img src="a.png" alt="user">'
+    name.innerHTML = 'John Doe'
+    date.innerHTML = 'Oct 08, 2020'
+
+
+    animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
+
+    animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-texts'))
+
+}
